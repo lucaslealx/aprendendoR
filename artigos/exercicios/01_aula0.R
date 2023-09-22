907f00b41d6b3ccb8dd900b1a634fda50416fcc0
Lucas Leal <124811327+lucaslealx@users.noreply.github.com>
2023-09-22 12:14:18 -0300

    Create 01_aula0.R

---
+++ artigos/exercicios/01_aula0.R
@@ -0,0 +1,51 @@
+# Cria a variável a com o valor 2.5
+a <- 2.5
+
+# E a variável b com valor 3
+b = 3
+
+# Agora crie a variável Valor (com V maiúsculo) com valor 5
+Valor <- 5
+
+# Qual o resultado de a + b?
+a+b
+
+# Faça a operação de valor (com v minúsculo) dividido por a
+# O que aconteceu?
+a/valor
+
+# Agora faça Valor / a 
+# Obs: a partir de agora, sempre vamos usar Valor (com v maiúsculo)
+Valor/a
+
+# E Valor / b
+Valor/b
+
+# Qual o inteiro da divisão de Valor / b?
+Valor %/% b
+
+# E o resto dessa divisão?
+Valor %% b
+
+# Qual seria o resultado se fizermos Valor elevado a b?
+Valor**b
+
+# Verifique agora se a é maior que b
+a > b
+
+# Então verifique se b é igual a 3
+b == 3
+
+# Verifique também se b é igual a 2.5
+b == 2.5
+
+# O que acontece se tentarmos verificar se b é igual a 2.5 fazendo b = 2.5?
+
+# Verifique agora se b é maior que a E menor que Valor
+b > a & b < Valor
+
+# E se b é maior que a OU b é maior que Valor
+b > a | b > Valor
+
+# Para fechar, verifique se b é maior que a E b é maior que Valor
+b > a & b > Valor
